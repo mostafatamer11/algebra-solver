f2827b0e793415ab01de34d1dbbc532c32a18e7a
Mostafa Tamer <mostafaghobashytamer@gmail.com>
2023-08-09 14:00:47 +0300

    initilized the repo and started the lexer

---
+++ Lexer/tokens.py
@@ -0,0 +1,41 @@
+"""
+# Tokens
+### The above class defines different types of tokens for algebriac expressions, including INT, VAR, OPER, etc.. for the Lexer
+"""
+
+EOFs = 0
+
+
+class Token(object):
+    def __init__(self, value:str):
+        global EOFs
+        if EOFs:
+            raise ExceptionGroup("EOF")
+        self.value = value
+
+    def __repr__(self) -> str:
+        return f"TOKEN({self.value}) "
+
+
+class INT(Token):
+    def __repr__(self) -> str:
+        return f"INT({self.value}) "
+
+
+class VAR(Token):
+    def __repr__(self) -> str:
+        return f"VAR({self.value}) "
+
+
+class OPER(Token):
+    def __repr__(self) -> str:
+        return "OPER(" + self.value + ") "
+
+
+class EOF(Token):
+    def __init__(self):
+        global EOFs
+        EOFs += 1
+
+    def __repr__(self):
+        return "EOF"
